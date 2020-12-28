e203e293fdc5109a96439b5f83c079d6d0b80991
dimon <d.korostelevdn@gmail.com>
2020-12-28 12:40:26 +0700

    run hist, clusters simultaneously

--- embed_pyqt.py
+++ embed_pyqt.py
@@ -39,14 +39,6 @@ class MainWindow(QMainWindow):
             bs.server.stop()
             bs.thread.join()
 
-
-        #  self.server.io_loop.stop()
-        # self.server.stop()
-        # x.join()
-
-
-
-
 class BokehServer(object):
 
     def __init__(self, server, thread):
@@ -59,6 +51,11 @@ class FormWidget(QWidget):
         super(FormWidget, self).__init__(parent)
         self.bokeh_server = None
         self.path_dict = dict()
+        self.port = 5006
+
+        self.hist_selector_name = 'Histogramm Selector'
+        self.clusters_selector_name = 'Clusters Selector'
+
         self.__layout()
 
 
@@ -67,7 +64,7 @@ class FormWidget(QWidget):
         user_path = self.path_dict[key].text()
 
         if len(user_path) != 0:
-            df_path = self.path_dict[key] 
+            df_path = user_path
 
         try:
             df_all = pd.read_csv(df_path)
@@ -88,10 +85,8 @@ class FormWidget(QWidget):
 
         map_cols = list(df.columns[3:])
         launcher = hist_cluster_selector(df=df, cols=map_cols, mode='notebook')
-        
-        res_name = '/'
-        self.bokeh_server = self.run_server(res_name, launcher)
-        self.run_browser(res_name)
+            
+        return launcher
 
 
 
@@ -118,22 +113,50 @@ class FormWidget(QWidget):
 
         launcher = cluster_selector(df=df, cols=new_cols, k_map=['X', 'Y'], k_emb=['F1', 'F2'], mode='notebook')
 
-        res_name = '/'
-        self.bokeh_server = self.run_server(res_name, launcher)
-        self.run_browser(res_name)
+        return launcher
+
+    def warn_dialog(self, msg):
+        msgBox = QMessageBox()
+        msgBox.setIcon(QMessageBox.Information)
+        msgBox.setText(msg)
+        msgBox.setWindowTitle("Info")
+        msgBox.setStandardButtons(QMessageBox.Ok)
+        msgBox.exec()
+
+
+    def __create_tabs(self):
+        hist_launcher = self.run_hist_selector()
+        clusters_launcher = self.run_cluster_selector()
+
+        if not hist_launcher:
+            self.warn_dialog(f"Can't read read csv file for {self.hist_selector_name}")
+            return
+
+        if not clusters_launcher:
+            self.warn_dialog(f"Can't read read csv file for {self.clusters_selector_name}")
+            return
+
+
+        launchers = {'/hist': hist_launcher, '/clusters': clusters_launcher}
+
+        self.bokeh_server = self.run_server(launchers)
+
+        for l in launchers.keys():
+            self.run_browser(l)
 
 
     def stop_server(self, bs):
         bs.server.io_loop.stop()
         bs.server.stop()
         bs.thread.join()
+        self.tab_widget.clear()
 
-    def run_server(self, res_name, launcher):
+    def run_server(self, launchers):
 
         if self.bokeh_server:
             self.stop_server(self.bokeh_server)
 
-        server = Server({res_name: launcher})
+        server = Server(launchers)
         server.start()
         x = threading.Thread(target=server.io_loop.start)
         x.start()
@@ -142,38 +165,40 @@ class FormWidget(QWidget):
 
     def run_browser(self, res_name):
 
-        self.browser.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.JavascriptEnabled, True)
+        dev = QWebEngineView()
+        browser = QWebEngineView()
 
-        self.browser.load(QUrl('http://localhost:5006' + res_name))
+        browser.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.JavascriptEnabled, True)
 
-        self.mainLayout = QtWidgets.QVBoxLayout()
-        self.webView = QtWebEngineWidgets.QWebEngineView()
-        self.mainLayout.addWidget(self.webView, 100)
-        self.webView.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.JavascriptEnabled, True)
-        self.webView.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.LocalContentCanAccessRemoteUrls,
+        browser.load(QUrl(f'http://localhost:{self.port}' + res_name))
+
+        mainLayout = QtWidgets.QVBoxLayout()
+        webView = QtWebEngineWidgets.QWebEngineView()
+        mainLayout.addWidget(webView, 100)
+        webView.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.JavascriptEnabled, True)
+        webView.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.LocalContentCanAccessRemoteUrls,
                                              True)
-        self.webView.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.ErrorPageEnabled, True)
-        self.webView.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled, True)
+        webView.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.ErrorPageEnabled, True)
+        webView.settings().setAttribute(QtWebEngineWidgets.QWebEngineSettings.PluginsEnabled, True)
 
         dev_view = QtWebEngineWidgets.QWebEngineView()
-        self.mainLayout.addWidget(dev_view, 100)
-        self.browser.page().setDevToolsPage(self.dev.page())
+        mainLayout.addWidget(dev_view, 100)
+        browser.page().setDevToolsPage(dev.page())
+        self.tab_widget.addTab(browser, res_name)
 
 
 
-    def __create_selector_block(self, run_btn_name, name, key, container, path_dict):
+    def __create_selector_block(self, name, key, container, path_dict):
         hbox = QHBoxLayout()
         label = QLabel(name)
         text_input = QLineEdit()
 
 
-        run_button = QPushButton(run_btn_name)
         button = QPushButton()
         # button.setIcon(QIcon(os.path.join('res', 'file_folder.png')))
         button.clicked.connect(partial(self.__on_choose_folder, key))
 
 
-        hbox.addWidget(run_button, stretch=50)
         hbox.addWidget(label, stretch=25)
         hbox.addWidget(text_input, stretch=25)
         hbox.addWidget(button)
@@ -181,7 +206,7 @@ class FormWidget(QWidget):
         container.addLayout(hbox)
         path_dict[key] = text_input
 
-        return run_button
+        return container
 
 
     def __on_choose_folder(self, name):
@@ -193,31 +218,18 @@ class FormWidget(QWidget):
     def __layout(self):
         self.vbox = QVBoxLayout()
         self.vbox_container = QVBoxLayout()
-        self.hist_selector_btn = QPushButton("Start hist selector")
-        self.cluster_selector_btn = QPushButton("Start cluster selector")
+        self.run_button = QPushButton("Run instruments")
+
+        self.__create_selector_block(f'{self.hist_selector_name} DataFrame:', 'hist', self.vbox_container, self.path_dict)
+        self.__create_selector_block(f'{self.clusters_selector_name} DataFrame:', 'cluster', self.vbox_container, self.path_dict)
+        self.vbox_container.addWidget(self.run_button)
 
 
-        self.hist_selector_btn = self.__create_selector_block('Start hist selector', \
-                                'DataFrame:', 'hist', self.vbox_container, self.path_dict)
-
-        self.cluster_selector_btn = self.__create_selector_block('Start cluster selector', \
-                                'DataFrame:', 'cluster', self.vbox_container, self.path_dict)
-
-
-
-        self.browser = QWebEngineView()
-        self.dev = QWebEngineView()
-
-        self.vbox_container.addWidget(self.browser, stretch=50)
-        self.vbox_container.addWidget(self.hist_selector_btn, stretch=10)
-        self.vbox_container.addWidget(self.cluster_selector_btn, stretch=10)
-
-        self.vbox.addLayout(self.vbox_container)
+        self.tab_widget = QTabWidget()
+        self.vbox.addLayout(self.vbox_container, stretch=10)
+        self.vbox.addWidget(self.tab_widget, stretch=50)
         self.setLayout(self.vbox)
-
-        self.cluster_selector_btn.clicked.connect(self.run_cluster_selector)
-        self.hist_selector_btn.clicked.connect(self.run_hist_selector)
-        
+        self.run_button.clicked.connect(self.__create_tabs)        
 
 
 def main():
